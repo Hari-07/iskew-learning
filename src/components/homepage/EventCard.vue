68ceeac0d450dedddda92f8182cc8b6e07b9690c
Harinandan <harinandan.alukkal@gmail.com>
2020-09-26 00:56:41 +0530

    card structure

--- src/components/homepage/EventCard.vue
+++ src/components/homepage/EventCard.vue
@@ -1,14 +1,27 @@
 <template>
 	<div id="card">
 		<div id="card2">
-
+			<div class="graphic" :class="programID">
+			</div>
+			<div class="programtitle">
+				<span class="programhl">12 Aptitude</span> <br>
+				Test Series
+			</div>
+			<div class="enrollbuttoncard">
+				<button class="cardbutton">
+					Buy Now
+				</button>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
 export default {
-
+	name:'EventCard',
+	props:{
+		programID:String,
+	}
 }
 </script>
 
@@ -33,21 +46,65 @@ export default {
 	background-size: 50%;
 	background-repeat: no-repeat;
 	background-position: top left;
+	padding-top:9%;
 }
 .cardImage{
 	height: 65%;
 }
-
 .cardTitle{
 	height: 35%;
 	background: white;
 	border-radius: 0 0 11px 11px;
 	box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1);
 }
+.graphic{
+	background: red;
+	height:46%;
+	margin-left: 56%;
+}
+
+.programtitle{
+	text-align: left;
+	margin-left: 15px;
+	font-size: 1.8vw;
+	line-height: 1.2;
+	font-weight: bold;
+}
+
+.programhl{
+	color:#0089E2;
+}
+
+.enrollbuttoncard{
+	text-align: left;
+	margin-left: 15px;
+	padding-top: 16%;
+}
+.cardbutton{
+	background:#FAFAFA;
+	border: inset 1.5px solid black;
+	border-radius: 11px;
+	padding: 7px 22px;
+	font-size:1.3vw;
+}
+.cardbutton:hover {
+	background: #0089E2;
+	color: white;
+}
 
 @media (max-width: 576px){
 	#card {
 		height: 70vw;
+	}
+
+	.programtitle{
+		font-size:5vw
+	}
+	.enrollbuttoncard{
+		padding-top:12%;
+	}
+	.cardbutton{
+		font-size: 4vw;
 	}
 }
 
