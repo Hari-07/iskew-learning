7dffbd306c2474ee9996b5f2f00500419e6c9b62
Harinandan <harinandan.alukkal@gmail.com>
2020-09-25 15:45:32 +0530

    footer

--- src/components/homepage/Footer.vue
+++ src/components/homepage/Footer.vue
@@ -1,10 +1,16 @@
 <template>
-	<div id="footer">
-		
+	<div class="footerarea">
+		<p class="footertext">Contact us on <span class="footerhl">+91 90209 37077</span> or via Email at <a href="mailto:iskewlearning@gmail.com" class="footerhl">iskewlearning@gmail.com</a></p>
+		<span class="footersociallink">
+			<a href="https://www.linkedin.com/company/i-skew/mycompany/"><dile-social-icon class="socialicon" icon="linkedin"></dile-social-icon></a>
+			<a href="https://www.instagram.com/i_skew/"><dile-social-icon class="socialicon" icon="instagram"></dile-social-icon></a>
+		</span>
 	</div>
 </template>
 
 <script>
+import 'social-icon/dile-social-icon.js'
+
 export default {
 	name:'Footer'
 }
@@ -12,4 +18,36 @@ export default {
 
 <style>
 
+.footerarea{
+	border-top:1px solid grey;
+	padding-top: 25px;
+	margin-bottom: 40px;
+	display: flex;
+	flex-direction: column;
+}
+
+.footertext{
+	font-size: 1.5vw;
+}
+
+.footerhl{
+	color: #0089E2;
+	font-weight:bold;
+}
+
+.footersociallink {
+	justify-content: center;
+	display: flex;
+	--dile-social-icon-color: #111111;
+}
+
+.socialicon{
+	padding:10px;
+}
+
+@media (max-width: 576px) {
+	.footertext{
+		font-size: 2.8vw;
+	}
+}
 </style>
