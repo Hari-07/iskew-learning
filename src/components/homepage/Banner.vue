432c71a38fcd18902ed106097bfa728da668db0b
Harinandan <harinandan.alukkal@gmail.com>
2020-10-15 23:43:30 +0530

    tech-banner-update

--- src/components/homepage/Banner.vue
+++ src/components/homepage/Banner.vue
@@ -1,57 +1,12 @@
 <template>
 	<div id="banner">
-			<!-- <div class="quoteBox">
-				<h1 class="bannerquote">
-					Adding Value to <br> your life <span style="color:#0089e2">since 2008</span>
-				</h1>
-				<b-button v-scroll-to="'#events' " variant="outline-secondary" class="enrollbutton" @click.prevent>
-					Enroll Now		
-				</b-button>
-			</div>
-			<div style="flex-grow:1"></div>
-			<img class="bannerimage" src="@/assets/bannerPic.png"> -->
-			<div class="tcssidebar">
-				<div class="element">
-					<img class="elementimage" src="@/assets/5.png">
-					<div class="elementtext">300+ TCS NQT Questions</div>
-				</div>
-				<div class="element">
-					<img class="elementimage" src="@/assets/4.png">
-					<div class="elementtext">12 Tests</div>
-				</div>
-				<div class="element">
-					<img class="elementimage" src="@/assets/3.png">
-					<div class="elementtext">Detailed Video Solutions</div>
-				</div>
-				<div class="element">
-					<img class="elementimage" src="@/assets/2.png">
-					<div class="elementtext">Expert Guidance</div>
-				</div>
-				<div class="element">
-					<img class="elementimage" src="@/assets/1.png">
-					<div class="elementtext">Live Zoom Training</div>
-				</div>
-			</div>
 			<div class="quoteBox">
 				<h1 class="bannerquote">
-					<span class="b1stats">300+ Paid Registrations for Batch 1<br></span>
-					<span style="color:white">TCS Aptitude Question <br> Paper Discussion <br> <span class="batch2">BATCH II</span></span>
+					<span style="color:#0089e2">Fundamentals of C Programming & Data Structures</span>
 				</h1>
-				<div class="bannerbuttons">
-					<a href="https://forms.gle/hyd895i85AsS1Vpp7">
-						<button variant="outline-secondary" class="enrollbutton">
-							Enroll Now		
-						</button>
-					</a>
-					<b-button v-scroll-to="{ el: '#events', offset: 400 }" variant="outline-secondary" class="moreprogrammesbutton" @click.prevent>
-						More Info		
-					</b-button>
-					<a href="https://drive.google.com/uc?export=download&id=1ZX5TJ1WeKzySAXviMmE7wHFi077vOVq1" target="_blank">
-						<button class="tcsschedulebutton">Click Here for Full Schedule</button>
-					</a>
-				</div>
+				<div class="bannersubquote">Oct 19-21, 6 hours</div>
 			</div>
-			<div style="flex-grow:1"></div>
+			<img class="bannerimage" src="@/assets/gummy.png">
 	</div>
 </template>
 
@@ -68,23 +23,24 @@ export default {
 	display: flex;
 	position: relative;
 	height: 35vw;
-	background-image: url('../../assets/BannerBG.jpg');
+	background-image: url('../../assets/whiteBG.jpg');
 	background-repeat: repeat;
 	background-origin: border-box;
 	background-size: cover;
 }
 
 .bannerimage {
-	height: 35vw;
+	height: 20vw;
 	float: right;
-	padding-right: 5vw;
+	margin-top:4vw;
+	margin-left: 17vw;
 }
 
 .quoteBox{
 	text-align: left;
-	/* min-width: 550px; */
 	margin-top:5vw ;
-	margin-left: 5vw;
+	margin-left: 8vw;
+	min-width: 35vw;
 }
 
 .bannerquote {
@@ -93,107 +49,10 @@ export default {
 	font-size: 4vw;
 }
 
-.batch2{
-	font-weight: 300;
-	color:grey;
-	font-size: 3vw;
-}
-
-.b1stats{
-	font-style: italic;
-	font-size: 1.8vw;
-	color: lightgrey;
-}
-
-.bannerbuttons {
-	margin-top: 15px;
-}
-
-.enrollbutton {
-	border:2px solid white;
-	color: white;
-	background: none;
-	font-size: 1.2vw;
-	border-radius: 11px;
-	padding: 0.5vw 30px;
-	height:3vw;
-}
-
-.enrollbutton:hover {
-	color:grey;
-	background: #111111;
-	border: 2px solid grey;
-}
-
-.enrollbutton:focus,:active {
-	outline: none;
-	box-shadow: none;
-}
-
-.moreprogrammesbutton {
-	border:2px solid white;
-	color: white;
-	background: none;
-
-	font-size: 1.2vw;
-	border-radius: 11px;
-	padding: 0.45vw 30px;
-	height:3vw;
-	margin-left: 2vw;
-}
-
-.moreprogrammesbutton:hover {
-	color:grey;
-	background: #111111;
-}
-
-.moreprogrammesbutton:focus,:active {
-	outline:none;
-	box-shadow: none;
-}
-
-.tcsschedulebutton {
-	color: white;
-	background: none;
-	border:none;
-	font-size: 1.2vw;
-	border-radius: 11px;
-	padding: 0.5vw 30px;
-	height:3vw;
-	margin-left: 0.5vw;
-}
-
-.tcsschedulebutton a:any-link{
-	color:white;
-}
-
-.tcsschedulebutton:hover {
-	color:grey;
-}
-
-.tcssidebar{
-	background: white;
-	height: 100%;
-	width: 8vw;
-	border-radius: 0 77px 77px 0;
-	box-shadow: 4px 2px 20px rgba(0, 0, 0, 0.2);
-	display: flex;
-	flex-direction: column;
-	padding: 2vw 0.8vw 2vw 0.2vw;
-}
-
-.element{
-	flex: 1;
-}
-
-.elementimage{
-	width: auto;
-	height: 3vw;
-}
-
-.elementtext{
-	font-size:0.8vw;
+.bannersubquote{
 	font-weight: bold;
+	font-size: 1.8vw;
+	font-style: italic;
 }
 
 @media (max-width: 576px){
@@ -201,17 +60,19 @@ export default {
 	#banner {
 		flex-direction: column;
 		align-items: center;
-		height: 100vw;
+		height: 95vw;
 	}
 
 	.bannerimage {
-		height: 70vw;
+		margin-left:0vw;
+		margin-top:0vw;
+		height: 35vw;
 		width: auto;
 	}
 
 	.quoteBox{
 	/* text-align: ; */
-		margin-top:12vw;
+		margin-top:8vw;
 		text-align: center;
 		margin-left: 0;
 		margin-bottom: 30px;
@@ -221,47 +82,8 @@ export default {
 		font-size: 8vw;
 	}
 
-	.batch2{
-		font-size: 6vw;
-	}
-
-	.b1stats{
-		font-size: 3.6vw;
-	}
-
-	.enrollbutton{
-		font-size: 4vw;
-		padding: 0.5vw 20px;
-		height:8vw;
-	}
-
-	.moreprogrammesbutton {
-		font-size: 4vw;
-		padding: 0.45vw 20px;
-		height:8vw;
-		margin-left: 2vw;
-	}
-
-	.tcsschedulebutton{
-		font-size: 4vw;
-		padding: 0.5vw 20px;
-		height:8vw;
-		margin-left: 0.5vw;
-	}
-
-	.tcssidebar{
-		flex-direction: row;
-		margin-top:5vw;
-		border-radius: 11px;
-		width: 70vw;
-	}
-
-	.elementimage{
-		height: 6vw;
-	}
-
-	.elementtext{
-		font-size: 1.7vw;
+	.bannersubquote{
+		font-size: 3.6vw
 	}
 }
 
--- src/router/index.js
+++ src/router/index.js
@@ -11,14 +11,6 @@ const routes = [
     component: Home
   },
   {
-    path: '/tcsschedule',
-    name: 'TcsSchedule',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/TcsSchedule.vue')
-  },
-  {
     path: '/thankyou',
     name: 'ThankYou',
     // route level code-splitting
