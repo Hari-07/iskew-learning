7e55d6db1afffc3defee6eb273ecd8420ec65f1c
Harinandan <harinandan.alukkal@gmail.com>
2020-10-17 17:40:01 +0530

    Batch 3 banner

--- src/components/homepage/Banner.vue
+++ src/components/homepage/Banner.vue
@@ -1,5 +1,15 @@
 <template>
 	<div id="banner">
+		<div class="banner2">
+			<div class="quoteBox2">
+				<h1 class="bannerquote2">
+					<div class="bannerbranding">iSkew Learning</div>
+					TCS NQT <span style="color:#0089e2">Aptitude Training</span>
+				</h1>
+				<div class="bannersubquote2"><span class="batchtag">BATCH III</span> <a href="https://forms.gle/7PTnDbiH5UPnrwQm9"><button class="enrollNow2">Enroll Now </button></a></div>
+			</div>
+		</div>
+		<div class="banner1">
 			<div class="quoteBox">
 				<h1 class="bannerquote">
 					<span style="color:#0089e2">TCS NQT Technical Training on C Programming</span>
@@ -7,6 +17,7 @@
 				<div class="bannersubquote">Oct 19-21, 6-8pm <a href="https://forms.gle/dyYPskeoP8Hidu1i6"><button class="enrollNow">Enroll Now </button></a></div>
 			</div>
 			<img class="bannerimage" src="@/assets/gummy.png">
+		</div>
 	</div>
 </template>
 
@@ -19,7 +30,7 @@ export default {
 
 <style>
 
-#banner {
+.banner1 {
 	display: flex;
 	position: relative;
 	height: 35vw;
@@ -27,6 +38,18 @@ export default {
 	background-repeat: repeat;
 	background-origin: border-box;
 	background-size: cover;
+	overflow: hidden;
+}
+
+.banner2{
+	display: flex;
+	position: relative;
+	height: 35vw;
+	background-image: url('../../assets/Batch3BG.jpg');
+	background-repeat: repeat;
+	background-origin: border-box;
+	background-size: cover;
+	overflow: hidden;
 }
 
 .bannerimage {
@@ -42,11 +65,29 @@ export default {
 	margin-left: 8vw;
 	min-width: 37vw;
 }
+.quoteBox2{
+	text-align: left;
+	margin-top:14vw ;
+	margin-left: 8vw;
+	min-width: 37vw;
+}
 
 .bannerquote {
 	font-weight: bold;
 	color: #111111;
 	font-size: 4vw;
+}
+.bannerquote2 {
+	font-weight: bold;
+	color: #111111;
+	font-size: 4vw;
+}
+
+.bannerbranding {
+	color: #0089e2;
+	font-size: 2vw;
+	font-style: italic;
+	line-height: 1;
 }
 
 .bannersubquote{
@@ -54,8 +95,28 @@ export default {
 	font-size: 1.8vw;
 	font-style: italic;
 }
+.bannersubquote2{
+	font-weight: bold;
+	font-size: 1.8vw;
+	color: white;
+	margin-left: 29.5vw;
+}
+
+.batchtag{
+	background: #111111;
+	padding: 4.5px 10px;
+}
 
 .enrollNow {
+	margin-left: 40px;
+	background: none;
+	border: 2px solid black;
+	border-radius: 11px;
+	font-size: 1.5vw;
+	padding: 0.4vw 1.5vw;
+	box-sizing: border-box;
+}
+.enrollNow2 {
 	margin-left: 40px;
 	background: none;
 	border: 2px solid black;
@@ -73,10 +134,15 @@ export default {
 
 @media (max-width: 576px){
 	
-	#banner {
+	.banner1 {
 		flex-direction: column;
 		align-items: center;
 		height: 95vw;
+	}
+	.banner2 {
+		flex-direction: column;
+		align-items: center;
+		height: 80vw;
 	}
 
 	.bannerimage {
@@ -93,18 +159,45 @@ export default {
 		margin-left: 0;
 		margin-bottom: 30px;
 	}
+	.quoteBox2{
+	/* text-align: ; */
+		margin-top:24vw;
+		text-align: center;
+		margin-left: 0;
+		margin-bottom: 30px;
+	}
+
+	.bannerbranding{
+		font-size:4vw;
+	}
 
 	.bannerquote{
+		font-size: 8vw;
+	}
+	.bannerquote2{
 		font-size: 8vw;
 	}
 
 	.bannersubquote{
 		font-size: 3.6vw
 	}
+	.bannersubquote2{
+		margin-left: 36vw;
+		width: 27vw;
+		font-size: 3.6vw;
+		display: flex;
+		flex-direction: column;
+	}
 
 	.enrollNow {
 		font-size: 3vw;
 		padding: 0.8vw 3vw;
+	}
+	.enrollNow2{
+		font-size: 3vw;
+		padding: 0.8vw 3vw;
+		margin-top: 3vw;
+		margin-left: 0;
 	}
 }
 
